19bba508a340a89f39f45e8d6348b26911421c7d
tarak <bouziri.tarak@gmail.com>
2021-05-06 13:56:58 -0400

    initialiser les pokemons avec useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Pokemon from "./models/pokemon"
 import POKEMONS from "./models/mock-pokemon";
 
 function App() {
 
-    const [pokemons] = useState(POKEMONS)
+    const [pokemons, setPokemon] = useState([]);
+
+    useEffect(() => {
+
+        fetch('https://limitless-reef-40594.herokuapp.com/api/pokemons')
+            .then(
+                response => {
+                    if (response.status !== 200) {
+                        console.log('Looks like there was a problem. Status Code: ' +
+                            response.status);
+                        return;
+                    }
+                    // Examine the text in the response
+                    response.json().then(function (data) {
+                        setPokemon(data.data)
+                    });
+                })
+            .catch(function (err) {
+                console.log('Fetch Error :-S', err);
+            })
+    }, [])
 
     return (
         <div>
