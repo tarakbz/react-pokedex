41e18003fc3c6a82ddba784877dc1de2769c9c8c
tarak <bouziri.tarak@gmail.com>
2021-05-06 20:23:46 -0400

    formater date

--- src/components/pokemon-card.js
+++ src/components/pokemon-card.js
@@ -12,18 +12,18 @@ export default function PokemonCard({pokemon, classes}) {
     const setDown = () => {
         setElevation(1)
     }
-
+    const formatDate = (date) => {
+        return new Intl.DateTimeFormat().format(new Date(date));
+        // on peut aussi faire
+        // const dateObject = new Date(date);
+        // return `${dateObject.getDate()}-${dateObject.getMonth() + 1 }-${dateObject.getFullYear()}`
+    }
     return (
         <Paper elevation={elevation} onMouseLeave={setDown} onMouseEnter={setUp}>
             <img src={pokemon.picture} alt=""/>
             <p className={classes.name}>
                 {pokemon.name} <br/>
-                <small>
-                    {
-                        new Intl.DateTimeFormat(
-                        ).format(new Date(pokemon.created))
-                    }
-                </small>
+                <small> {formatDate(pokemon.created)}</small>
             </p>
         </Paper>
     );
