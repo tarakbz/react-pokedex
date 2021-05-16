b47831b229fea97290b50edafed1be3edc8e8438
tarak <bouziri.tarak@gmail.com>
2021-05-15 23:06:49 -0400

    ajout de erreurs basiques

--- src/services/pokemon-service.js
+++ src/services/pokemon-service.js
@@ -3,17 +3,22 @@
  */
 export default class PokemonService {
 
-
     static getPokemons() {
         return fetch('https://limitless-reef-40594.herokuapp.com/api/pokemons')
             .then(response => response.json())
             .then(json => json.data)
+            .catch(error => this.handleError(error));
     }
 
     static getPokemon(id) {
         return fetch(`https://limitless-reef-40594.herokuapp.com/api/pokemons/${id}`)
             .then(response => response.json())
             .then(json => json.data)
+            .catch(error => this.handleError(error));
+    }
+
+    static handleError(error) {
+        console.error(error);
     }
 
 }
