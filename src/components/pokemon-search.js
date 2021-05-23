d7296e83a8897e0bf7dcda0980afc4e76f1fc440
tarak <bouziri.tarak@gmail.com>
2021-05-22 22:40:40 -0400

    Revert "test app bar simple"
    This reverts commit b37232ff82cff7c45ace9b147c102fd86b00e067.

--- src/components/pokemon-search.js
+++ src/components/pokemon-search.js
@@ -1,42 +1,61 @@
-// import {useState} from "react";
+import {useState} from "react";
 import InputBase from '@material-ui/core/InputBase';
 import SearchIcon from '@material-ui/icons/Search';
-// import Autocomplete from '@material-ui/lab/Autocomplete';
-// import {Link} from "react-router-dom";
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import {Link} from "react-router-dom";
 
-// import PokemonService from "../services/pokemon-service";
+import PokemonService from "../services/pokemon-service";
 import style from "../styles/style";
-// import {Paper} from "@material-ui/core";
+import {Paper} from "@material-ui/core";
 
 
 const PokemonSearch = () => {
     const classes = style();
 
-    // const [term, setTerm] = useState('');
-    // const [pokemons, setPokemons] = useState([])
-    //
-    // function handleInputChange(e) {
-    //     const term = e.target.value;
-    //     setTerm(term);
-    //     if (term.length <= 1) {
-    //         setPokemons([]);
-    //         return
-    //     }
-    //     PokemonService.searchPokemons(term).then(pokemons => setPokemons(pokemons));
-    // }
+    const [term, setTerm] = useState('');
+    const [pokemons, setPokemons] = useState([])
+
+    function handleInputChange(e) {
+        const term = e.target.value;
+        setTerm(term);
+        if (term.length <= 1) {
+            setPokemons([]);
+            return
+        }
+        PokemonService.searchPokemons(term).then(pokemons => setPokemons(pokemons));
+    }
 
     return (
         <div className={classes.search}>
             <div className={classes.searchIcon}>
-                <SearchIcon />
+                <SearchIcon/>
             </div>
-            <InputBase
-                placeholder="Search…"
-                classes={{
-                    root: classes.inputRoot,
-                    input: classes.inputInput,
-                }}
-                inputProps={{ 'aria-label': 'search' }}
+            <Autocomplete
+                freeSolo
+                debug
+                disableClearable
+                options={pokemons}
+                getOptionLabel={(pokemon) => pokemon.name}
+                renderOption={(pokemon) =>
+                    <Paper component={Link} className={classes.autocompleteItem} to={`/pokemons/${pokemon.id}`}>
+                        <img className={classes.autocompleteImage} src={pokemon.picture} alt=""/>
+                        {pokemon.name}
+                    </Paper>
+                }
+                renderInput={(params) => (
+                    <div ref={params.InputProps.ref}>
+                        <InputBase
+                            placeholder="Search..."
+                            {...params.inputProps}
+                            classes={{
+                                root: classes.inputSearchRoot,
+                                input: classes.inputSearchInput,
+                            }}
+                            value={term}
+                            onChange={e => handleInputChange(e)}
+                        />
+                    </div>
+                )}
             />
         </div>
     );
--- src/styles/style.js
+++ src/styles/style.js
@@ -17,13 +17,7 @@ const style = makeStyles(() => ({
         backgroundColor: fade(theme.palette.common.white, 0.15),
         '&:hover': {
             backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(1),
-            width: 'auto',
-        },
+        }
     },
     searchIcon: {
         padding: theme.spacing(0, 2),
@@ -34,21 +28,22 @@ const style = makeStyles(() => ({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    inputRoot: {
+    autocompleteItem: {
+        color: theme.palette.text.primary,
+        textDecoration: "none",
+        textAlign: "center"
+    },
+    autocompleteImage: {
+        width: "100%",
+        display: "block"
+    },
+    inputSearchRoot: {
         color: 'inherit',
     },
-    inputInput: {
+    inputSearchInput: {
         padding: theme.spacing(1, 1, 1, 0),
         // vertical padding + font size from searchIcon
         paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            width: '12ch',
-            '&:focus': {
-                width: '20ch',
-            },
-        },
     },
     menuButton: {
         color: "white"
