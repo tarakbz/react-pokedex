b37232ff82cff7c45ace9b147c102fd86b00e067
tarak <bouziri.tarak@gmail.com>
2021-05-22 22:40:40 -0400

    test app bar simple

--- src/styles/style.js
+++ src/styles/style.js
@@ -17,7 +17,13 @@ const style = makeStyles(() => ({
         backgroundColor: fade(theme.palette.common.white, 0.15),
         '&:hover': {
             backgroundColor: fade(theme.palette.common.white, 0.25),
-        }
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
     },
     searchIcon: {
         padding: theme.spacing(0, 2),
@@ -28,22 +34,21 @@ const style = makeStyles(() => ({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    autocompleteItem: {
-        color: theme.palette.text.primary,
-        textDecoration: "none",
-        textAlign: "center"
-    },
-    autocompleteImage: {
-        width: "100%",
-        display: "block"
-    },
-    inputSearchRoot: {
+    inputRoot: {
         color: 'inherit',
     },
-    inputSearchInput: {
+    inputInput: {
         padding: theme.spacing(1, 1, 1, 0),
         // vertical padding + font size from searchIcon
         paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
     },
     menuButton: {
         color: "white"
